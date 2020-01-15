1fa1e654f605c5a13aaa0ac2f243568a95db14e6
Haohan He <hhh910103@163.com>
2020-01-15 22:33:13 +0800

    goodtogo

--- js/index.js
+++ js/index.js
@@ -82,7 +82,7 @@ $(document).ready(function() {
 
         var qrImg = document.getElementById("temp");
         qrImg.crossOrigin = 'Anonymous';
-        qrImg.src = 'https://www.kuaizhan.com/common/encode-png?large=true&data=https://hehaohan.github.io/qrpay/qr.html?ali='+ali+'%26qq='+qq+'%26vx='+vx+'%26jd='+jd+'%26bd='+bd+'%26uin='+uin;
+        qrImg.src = 'https://api.isoyu.com/qr/?m=2&e=L&p=6&url=https://hehaohan.github.io/qrpay/qr.html?ali='+ali+'%26vx='+vx+'%26qq='+qq+'%26jd='+jd+'%26bd='+bd+'%26PayPal='+PayPal+'%26uin='+uin;
         $(qrImg).load(function(){
             setTimeout(resetCanvas(data,tpl_id,loading),500);
         });
